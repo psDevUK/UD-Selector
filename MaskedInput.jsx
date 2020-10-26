d2cf6765462aef3cef835074a18e08cbf1085fe2
Adam <adambacon1@hotmail.com>
2020-10-26 21:30:30 +0000

    For Pal

---
+++ MaskedInput.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import MaskedInput from 'react-text-mask'
+class MaskedInput extends React.Component {
+
+  // state is for keeping control state before or after changes.
+  state = {
+    // the things you want to put in state.
+    // text: this.props.text //un comment the line to use state insted props
+  }
+
+
+  render() {
+
+    return (
+      <MaskedInput
+        mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+        className="form-control"
+        placeholder="Enter a phone number"
+        guide={false}
+      // onBlur={() => { }}
+      // onChange={() => { }}
+      />
+    );
+
+  }
+}
+
+export default MaskedInput
